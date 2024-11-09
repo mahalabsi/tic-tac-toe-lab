fa384c367193e9cbaab0a923a0e796c4e84e6d12
mahalabsi <mahatalabsi@gmail.com>
2024-11-09 09:19:48 +0300

    update

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const choices = ['X', 'O']
 /*---------------------------- Variables (state) ----------------------------*/
 //1) Define the required variables used to track the state of the game.
-let board
-let turn
-let winner
-let tie
+let board = ['', '', '', '', '', '', '', '', '']
+let turn = 'X'
+let winner = false
+let tie = false
 
 /*------------------------ Cached Element References ------------------------*/
 //2) Store cached element references.
@@ -18,33 +18,44 @@ const squareEls = document.querySelector('.board')
 
 const messageEl = document.querySelector('#message')
 
-// console.log(squareEls)
-// console.log(messageEl)
+console.log(squareEls)
+console.log(messageEl)
 
 /*-------------------------------- Functions --------------------------------*/
 //3) Upon loading, the game state should be initialized, and a function should
 //   be called to render this game state.
+
 // a. Create a function called init.
 // b. Call the init function when the app loads.
-const init = () => {}
-console.log(init)
 // c. Set the board variable to an array containing nine empty strings ('') representing empty squares.
-const board = ['', '', '', '', '', '', '', '', '']
 // d. Set the turn to X - this will represent player X.
-let turn = 'X'
 // 💡 Player O will be represented by O.
-
 // e. Set the winner to false.
-let winner = false
 // 💡 A false value in winner means that there is no winner yet. A value of true in winner will mean that a player has won.
-
 // Once winner is set to true, we can determine which player won by whose turn it was when the winning move was played.
-
 // f. Set tie to false.
-let tie = false
 // 💡 A true value in tie will mean that the board array contains no more empty strings ('') and will be used to render a tie message if winner is still false by the time all squares are played.
-
 // g. Call a function named render() at the end of the init() function.
+
+const init = () => {
+  // render () => {
+  //   console.log('The Game Has Started!')
+  // }
+}
+
+// const updateBoard = () => {
+
+// }
+
+const updateBoard = () => {
+  squareEls.forEach((squareEl, index) => {
+    squareEl.textContent = board[index]
+    squareEl.classList.remove('X', 'O')
+    if (board[index] !== '') {
+      squareEl.classList.add(board[index])
+    }
+  })
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
